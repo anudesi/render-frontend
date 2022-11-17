ede7f418a750e0a82bea802e21481bba7cd1554d
anu <anupama.desikan@gmail.com>
2022-11-17 15:09:43 +0100

    updated

--- src/components/alluser.js
+++ src/components/alluser.js
@@ -8,7 +8,7 @@ function Getallusers(props){
     })
     useEffect(()=>{
         try{
-            axios.get('http://localhost:4000/getallusers').then(res=>{
+            axios.get('https://user-app-example-back.onrender.com/getallusers').then(res=>{
                 setuser({
                     students:res.data
                 });
